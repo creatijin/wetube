5705541fff88ef6ee764470aaf6f47bf1a040bb6
creatijin <newlex0124@gmail.com>
2020-01-13 18:08:12 +0900

    2.4 Handling Routes with Express / next 2.5

--- index.js
+++ index.js
@@ -8,4 +8,16 @@ function handleListening() {
   console.log(`Listening on: http://Localhost:${PORT}`)
 }
 
+function handleHome(req, res) {
+  console.log("Hi from Home!!");
+}
+
+function handleProfile(req, res) {
+  console.log("You are on my profile")
+}
+
+app.get("/", handleHome);
+
+app.get("/profile", handleProfile);
+
 app.listen(PORT, handleListening);
